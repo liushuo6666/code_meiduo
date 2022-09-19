f6595784901eb9ee1350dbb606e34d88bbbcdc81
liushuo6666 <lshpnn@163.com>
2022-09-19 20:39:03 +0800

    '创建后端代码'

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -1,4 +1,4 @@
-"""meiduo_01 URL Configuration
+"""meiduo_mall URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
